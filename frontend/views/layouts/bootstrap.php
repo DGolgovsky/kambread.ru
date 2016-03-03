efb929f328c363cfef14c8aa7a95f53e05ec6f8c
DGolgovsky <d.westcoast@aol.com>
2016-03-03 12:34:40 +0300

    beautify head

--- frontend/views/layouts/bootstrap.php
+++ frontend/views/layouts/bootstrap.php
@@ -1,18 +1,17 @@
-<?
+<?php
 use yii\helpers\Html;
 use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
 \frontend\assets\MainAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?= Yii::$app->language ?>">
 <head>
-    <title>
-        <?= $this->title ?>
-    </title>
     <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <?= yii\helpers\Html::csrfMetaTags() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
 
