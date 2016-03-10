f9b081e661c21f90856a4f66bd8dffa36f2ce206
Dmitry Golgovsky <d.westcoast@aol.com>
2016-03-10 15:54:10 +0300

    advert model generate

---
+++ frontend/models/Advert.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "advert".
+ *
+ * @property integer $id
+ * @property integer $price
+ * @property string $address
+ * @property integer $fk_agent_detail
+ * @property integer $bedroom
+ * @property integer $livingroom
+ * @property integer $parking
+ * @property integer $kitchen
+ * @property string $general_image
+ * @property string $description
+ * @property string $location
+ * @property integer $hot
+ * @property integer $sold
+ * @property string $type
+ * @property integer $recommend
+ * @property integer $created_at
+ * @property integer $updated_at
+ */
+class Advert extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'advert';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['price', 'fk_agent_detail', 'bedroom', 'livingroom', 'parking', 'kitchen', 'hot', 'sold', 'recommend', 'created_at', 'updated_at'], 'integer'],
+            [['description'], 'string'],
+            [['address'], 'string', 'max' => 255],
+            [['general_image'], 'string', 'max' => 200],
+            [['location'], 'string', 'max' => 30],
+            [['type'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'price' => 'Price',
+            'address' => 'Address',
+            'fk_agent_detail' => 'Fk Agent Detail',
+            'bedroom' => 'Bedroom',
+            'livingroom' => 'Livingroom',
+            'parking' => 'Parking',
+            'kitchen' => 'Kitchen',
+            'general_image' => 'General Image',
+            'description' => 'Description',
+            'location' => 'Location',
+            'hot' => 'Hot',
+            'sold' => 'Sold',
+            'type' => 'Type',
+            'recommend' => 'Recommend',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     * @return AdvertQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new AdvertQuery(get_called_class());
+    }
+}
---
+++ frontend/models/AdvertQuery.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\models;
+
+/**
+ * This is the ActiveQuery class for [[Advert]].
+ *
+ * @see Advert
+ */
+class AdvertQuery extends \yii\db\ActiveQuery
+{
+    /*public function active()
+    {
+        return $this->andWhere('[[status]]=1');
+    }*/
+
+    /**
+     * @inheritdoc
+     * @return Advert[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * @inheritdoc
+     * @return Advert|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+}
