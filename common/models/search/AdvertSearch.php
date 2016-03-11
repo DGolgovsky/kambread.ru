6e2da6e60cb3560bc72f0a47ade9825d07e03fd4
DGolgovsky <d.westcoast@aol.com>
2016-03-11 14:55:17 +0300

    connect with db

--- common/models/search/AdvertSearch.php
+++ common/models/search/AdvertSearch.php
@@ -12,70 +12,70 @@ use common\models\Advert;
  */
 class AdvertSearch extends Advert
 {
-    /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return [
-            [['idadvert', 'price', 'fk_agent', 'bedroom', 'livingroom', 'parking', 'kitchen', 'hot', 'sold', 'recommend', 'created_at', 'updated_at'], 'integer'],
-            [['address', 'general_image', 'description', 'location', 'type'], 'safe'],
-        ];
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function rules()
+	{
+		return [
+			[['idadvert', 'price', 'fk_agent', 'bedroom', 'livingroom', 'parking', 'kitchen', 'hot', 'sold', 'recommend', 'created_at', 'updated_at'], 'integer'],
+			[['address', 'general_image', 'description', 'location', 'type'], 'safe'],
+		];
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function scenarios()
-    {
-        // bypass scenarios() implementation in the parent class
-        return Model::scenarios();
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function scenarios()
+	{
+		// bypass scenarios() implementation in the parent class
+		return Model::scenarios();
+	}
 
-    /**
-     * Creates data provider instance with search query applied
-     *
-     * @param array $params
-     *
-     * @return ActiveDataProvider
-     */
-    public function search($params)
-    {
-        $query = Advert::find();
+	/**
+	 * Creates data provider instance with search query applied
+	 *
+	 * @param array $params
+	 *
+	 * @return ActiveDataProvider
+	 */
+	public function search($params)
+	{
+		$query = Advert::find();
 
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-        ]);
+		$dataProvider = new ActiveDataProvider([
+			'query' => $query,
+		]);
 
-        $this->load($params);
+		$this->load($params);
 
-        if (!$this->validate()) {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
-            return $dataProvider;
-        }
+		if (!$this->validate()) {
+			// uncomment the following line if you do not want to return any records when validation fails
+			// $query->where('0=1');
+			return $dataProvider;
+		}
 
-        $query->andFilterWhere([
-            'idadvert' => $this->idadvert,
-            'price' => $this->price,
-            'fk_agent' => $this->fk_agent,
-            'bedroom' => $this->bedroom,
-            'livingroom' => $this->livingroom,
-            'parking' => $this->parking,
-            'kitchen' => $this->kitchen,
-            'hot' => $this->hot,
-            'sold' => $this->sold,
-            'recommend' => $this->recommend,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-        ]);
+		$query->andFilterWhere([
+			'idadvert' => $this->idadvert,
+			'price' => $this->price,
+			'fk_agent' => $this->fk_agent,
+			'bedroom' => $this->bedroom,
+			'livingroom' => $this->livingroom,
+			'parking' => $this->parking,
+			'kitchen' => $this->kitchen,
+			'hot' => $this->hot,
+			'sold' => $this->sold,
+			'recommend' => $this->recommend,
+			'created_at' => $this->created_at,
+			'updated_at' => $this->updated_at,
+		]);
 
-        $query->andFilterWhere(['like', 'address', $this->address])
-            ->andFilterWhere(['like', 'general_image', $this->general_image])
-            ->andFilterWhere(['like', 'description', $this->description])
-            ->andFilterWhere(['like', 'location', $this->location])
-            ->andFilterWhere(['like', 'type', $this->type]);
+		$query->andFilterWhere(['like', 'address', $this->address])
+			->andFilterWhere(['like', 'general_image', $this->general_image])
+			->andFilterWhere(['like', 'description', $this->description])
+			->andFilterWhere(['like', 'location', $this->location])
+			->andFilterWhere(['like', 'type', $this->type]);
 
-        return $dataProvider;
-    }
+		return $dataProvider;
+	}
 }
