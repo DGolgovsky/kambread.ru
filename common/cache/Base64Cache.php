be243b38d66b50064fd7003565fa0a66ef2540e9
DGolgovsky <d.westcoast@aol.com>
2016-03-14 14:31:08 +0300

    add fileCache

---
+++ common/cache/Base64Cache.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace common\cache;
+
+use yii\caching\FileCache;
+
+class Base64Cache extends FileCache
+{
+    public $cacheFileSuffix = '.base64';
+
+    protected function getValue($key)
+    {
+        $value = base64_decode(parent::getValue($key));
+        return $value;
+    }
+
+    protected function setValue($key, $value, $duration)
+    {
+        $value = base64_encode($value);
+        parent::setValue($key,$value,$duration);
+    }
+}
