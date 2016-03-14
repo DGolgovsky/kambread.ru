32c8414ec4b839bde10b63a1629fbe78134fcf5d
DGolgovsky <d.westcoast@aol.com>
2016-03-14 12:30:18 +0300

    slider uses uploaded images

--- frontend/components/Common.php
+++ frontend/components/Common.php
@@ -5,40 +5,40 @@ use yii\base\Component;
 
 class Common extends Component
 {
-    const EVENT_NOTIFY = 'notify_admin';
+	const EVENT_NOTIFY = 'notify_admin';
 
-    public function sendMail($name, $email, $subject, $body) {
-        if(\Yii::$app->mail->compose()
-            ->setFrom(['web.notify@kambread.ru' => $email])
-            ->setTo([\Yii::$app->params['supportEmail'] => $name])
-            ->setSubject($subject)
-            ->setTextBody($body)
-            ->send()) {
-            $this->trigger(self::EVENT_NOTIFY);
-            return true;
-        }
-    }
+	public function sendMail($name, $email, $subject, $body) {
+		if(\Yii::$app->mail->compose()
+			->setFrom(['web.notify@kambread.ru' => $email])
+			->setTo([\Yii::$app->params['supportEmail'] => $name])
+			->setSubject($subject)
+			->setTextBody($body)
+			->send()) {
+			$this->trigger(self::EVENT_NOTIFY);
+			return true;
+		}
+	}
 
-    public function notifyAdmin($event) {
-        print "Notify Admin";
-    }
+	public function notifyAdmin($event) {
+		print "Notify Admin";
+	}
 
-    public static function getTitleAdvert($data) {
-        return $data['bedroom'].' Bed Rooms and '.$data['kitchen'].' Kitchen Room Aparment on Sale';
-    }
+	public static function getTitleAdvert($data) {
+		return $data['bedroom'].' Bed Rooms and '.$data['kitchen'].' Kitchen Room Aparment on Sale';
+	}
 
-    public static function getImageAdvert($data,$general = true,$original = false) {
-        $image = [];
-        $base = Url::base();
-        if($original) {
-            $image[] = $base.'uploads/adverts/'.$data['idadvert'].'/general/'.$data['general_image'];
-        } else {
-            $image[] = $base.'uploads/adverts/'.$data['idadvert'].'/general/small_'.$data['general_image'];
-        }
-        return $image;
-    }
+	public static function getImageAdvert($data,$general = true,$original = false) {
+		$image = [];
+		$base = Url::base();
+		if($original) {
+			$image[] = $base.'uploads/adverts/'.$data['idadvert'].'/general/'.$data['general_image'];
+		} else {
+			$image[] = $base.'uploads/adverts/'.$data['idadvert'].'/general/small_'.$data['general_image'];
+		}
+		return $image;
+	}
 
-    public static function substr($text, $start=0, $end=50) {
-        return mb_substr($text,$start,$end);
-    }
+	public static function substr($text, $start=0, $end=50) {
+		return mb_substr($text,$start,$end);
+	}
 }
--- frontend/modules/main/controllers/DefaultController.php
+++ frontend/modules/main/controllers/DefaultController.php
@@ -4,45 +4,63 @@ namespace app\modules\main\controllers;
 
 use frontend\components\Common;
 use yii\web\Controller;
+use yii\db\Query;
 
 class DefaultController extends Controller
 {
-  public function actionIndex() {
-    $this->layout = "bootstrap";
-    return $this->render('index');
-  }
+	public function actionIndex() {
+		$this->layout = "bootstrap";
+		$query = new Query();
+		$command = $query->from('advert')->orderBy('idadvert desc')->limit(5);
+		$result_general = $command->all();
+		$count_general = $command->count();
+		
+		return $this->render('index',['result_general' => $result_general, 'count_general' => $count_general]);
+	}
+	
+	public function actionService() {
+		$locator = \Yii::$app->locator;
+		$cache = $locator->cache;
+		$cache->set("test",1);
+		
+		print $cache->get("test");
+	}
+	
+	public function actionEvent() {
+		$component = \Yii::$app->common; //new Common();
+		$component->on(Common::EVENT_NOTIFY,[$component,'notifyAdmin']);
+		$component->sendMail("tech@kambread.ru","Уведомление","Текст уведомления");
+		$component->off(Common::EVENT_NOTIFY,[$component,'notifyAdmin']);
+	}
+	
+	public function actionPath() {
+		// @yii
+        // @app
+        //@runtime
+        //@webroot
+        //@web
+        //@vendor
+        //@bower
+        //@npm
+        // @frontend
+        // @backend
 
-  public function actionService() {
-    $locator = \Yii::$app->locator;
-    $cache = $locator->cache;
+        //print \Yii::getAlias('@test');
 
-    $cache->set("test", 1);
-    print $cache->get("test");
-  }
+    }
 
-  public function actionEvent() {
-    $component = \Yii::$app->common; //new Common();
-    $component->on(Common::EVENT_NOTIFY,[$component,'notifyAdmin']);
-    $component->sendMail("tech@kambread.ru","Уведомление","Текст уведомления");
-    $component->off(Common::EVENT_NOTIFY,[$component,'notifyAdmin']);
-  }
+    public function actionCacheTest(){
 
-  public function actionPath() {
-    // @yii - framework path
-    // @app - back/front
-    // @runtime - back/front runtime
-    // @webroot - front/back/web
-    // @web - url to web folder
-    // @vendor
-    // @bower
-    // @npm
-    // @frontend
-    // @backend
+        $locator = \Yii::$app->locator;
+        $locator->cache->set('test',1);
 
-    // print \Yii::getAlias('@debugPath');
-  }
-  public function actionLoginData() {
-    print \Yii::$app->user->id;
-    print \Yii::$app->user->identity->username;
-  }
+        print   $locator->cache->get('test');
+
+
+    }
+
+    public function actionLoginData(){
+
+        print \Yii::$app->user->identity->username;
+    }
 }
