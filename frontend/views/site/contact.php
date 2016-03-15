ff1ad59a3ac69d1943c4c103f3f3b63a300906e0
Dmitry Golgovsky <d.westcoast@aol.com>
2016-03-15 22:13:46 +0300

    add to svn uploads

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -32,7 +32,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
                 <?= $form->field($model, 'verifyCode')->widget(\yii\captcha\Captcha::className(), [
                     'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-                    'captchaAction' => \yii\helpers\Url::to(['main/captcha'])
+                    'captchaAction' => \yii\helpers\Url::to(['site/captcha'])
                 ]) ?>
 
                 <div class="form-group">
