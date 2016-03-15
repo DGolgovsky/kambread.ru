085aa3da841af019db78320ec28e19e4e76cdb14
DGolgovsky <d.westcoast@aol.com>
2016-03-15 14:27:18 +0300

    detailes view

--- frontend/modules/main/controllers/MainController.php
+++ frontend/modules/main/controllers/MainController.php
@@ -1,16 +1,20 @@
 <?php
 namespace app\modules\main\controllers;
 
+use common\models\Advert;
 use common\models\LoginForm;
+use frontend\filters\FilterAdvert;
 use frontend\models\ContactForm;
 use frontend\models\Image;
 use frontend\models\SignupForm;
+use Yii;
+use yii\base\DynamicModel;
 use yii\web\Response;
 use yii\widgets\ActiveForm;
 
 class MainController extends \yii\web\Controller
 {
-	public $layout = 'inner';
+	public $layout = "inner";
 
 	public function actions()
 	{
@@ -21,32 +25,42 @@ class MainController extends \yii\web\Controller
 			],
 			'test' => [
 				'class' => 'frontend\actions\TestAction',
-			],
+			]
+		];
+	}
+
+	public function behaviors()
+	{
+		return [
+			[
+				'only' => ['view-advert'],
+				'class' => FilterAdvert::className(),
+			]
 		];
 	}
 
 	public function actionIndex()
 	{
-		$this->layout = "bootstrap";
-		return $this->redirect('/default/index');
+		return $this->render('index');
 	}
 
 	public function actionRegister()
 	{
+
 		$model = new SignupForm();
 		// 1st: $model = new SignupForm(['scenario' => 'short_u_e_p']);
 		// 2nd: $model->scenario = 'short_u_e_p'; // валидация по сценарию
 
-		if(\Yii::$app->request->isAjax && \Yii::$app->request->isPost){
-			if($model->load(\Yii::$app->request->post())) {
-				\Yii::$app->response->format = Response::FORMAT_JSON;
+		if(Yii::$app->request->isAjax && Yii::$app->request->isPost){
+			if($model->load(Yii::$app->request->post())) {
+				Yii::$app->response->format = Response::FORMAT_JSON;
 				return ActiveForm::validate($model);
 			}
 		}
 
-		if($model->load(\Yii::$app->request->post()) && $model->signup()){
+		if($model->load(Yii::$app->request->post()) && $model->signup()){
 
-			\Yii::$app->session->setFlash('success', 'Register Success');
+			Yii::$app->session->setFlash('success', 'Register Success');
 		}
 
 		return $this->render("register",['model' => $model]);
@@ -56,7 +70,7 @@ class MainController extends \yii\web\Controller
 	{
 		$model = new LoginForm;
 
-		if($model->load(\Yii::$app->request->post()) && $model->login()) {
+		if($model->load(Yii::$app->request->post()) && $model->login()) {
 			$this->goBack();
 		}
 
@@ -65,18 +79,18 @@ class MainController extends \yii\web\Controller
 
 	public function actionLogout()
 	{
-		\Yii::$app->user->logout();
+		Yii::$app->user->logout();
 		return $this->goHome();
 	}
 
 	public function actionContact()
 	{
 		$model = new ContactForm();
-		if($model->load(\Yii::$app->request->post()) && $model->validate()) {
+		if($model->load(Yii::$app->request->post()) && $model->validate()) {
 			$body = " <div>Сообщение от: <b> ".$model->name." </b></div>";
 			$body .= " <div>Email: <b> ".$model->email." </b></div>";
 			$body .= " <div>Текст: <b> ".$model->body." </b></div>";
-			\Yii::$app->common->sendMail(
+			Yii::$app->common->sendMail(
 				$model->name,
 				$model->email,
 				$model->subject,
@@ -86,5 +100,41 @@ class MainController extends \yii\web\Controller
 			die;
 		}
 		return $this->render("contact", ['model' => $model]);
+	}
+
+	public function actionViewAdvert($id)
+	{
+		$model = Advert::findOne($id);
+
+		$data = ['name', 'email', 'text'];
+		$model_feedback = new DynamicModel($data);
+		$model_feedback->addRule('name','required');
+		$model_feedback->addRule('email','required');
+		$model_feedback->addRule('text','required');
+		$model_feedback->addRule('email','email');
+
+		if(Yii::$app->request->isPost) {
+			if ($model_feedback->load(Yii::$app->request->post()) && $model_feedback->validate()) {
+				Yii::$app->common->sendMail($model_feedback->name, $model_feedback->email, "По ".$model->id,$model_feedback->text);
+			}
+		}
+
+		$user = $model->user;
+		$images = \frontend\components\Common::getImageAdvert($model,false);
+
+		$current_user = ['email' => '', 'username' => ''];
+
+		if(!Yii::$app->user->isGuest) {
+			$current_user['email'] = Yii::$app->user->identity->email;
+			$current_user['username'] = Yii::$app->user->identity->username;
+		}
+
+		return $this->render('view_advert',[
+			'model' => $model,
+			'model_feedback' => $model_feedback,
+			'user' => $user,
+			'images' =>$images,
+			'current_user' => $current_user
+		]);
 	}
 }
---
+++ frontend/modules/main/views/main/view_advert.php
@@ -0,0 +1,83 @@
+<div class="row">
+    <div class="col-lg-3 col-sm-4 hidden-xs">
+        <?php echo \frontend\widgets\HotWidget::widget() ?>
+    </div>
+    <div class="col-lg-9 col-sm-8 ">
+        <h2><?=\frontend\components\Common::getTitleAdvert($model) ?></h2>
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="property-images">
+                    <!-- Slider Starts -->
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators hidden-xs">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <?php
+                            foreach(range(1,count($images) - 1) as $s):
+                                ?>
+                                <li data-target="#myCarousel" data-slide-to="<?=$s ?>" class=""></li>
+                            <?php
+                            endforeach;
+                            ?>
+                        </ol>
+                        <div class="carousel-inner">
+                            <!-- Item 1 -->
+                            <div class="item active">
+                                <img src="<?=\frontend\components\Common::getImageAdvert($model)[0] ?>"  class="properties" alt="properties" />
+                            </div>
+                            <?php
+                            foreach($images as $image):
+                                ?>
+                                <div class="item">
+                                    <img src="<?=$image ?>"  class="properties" alt="properties" />
+                                </div>
+                            <?php
+                            endforeach
+                            ?>
+                        </div>
+                        <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+                        <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+                    </div>
+                    <!-- #Slider Ends -->
+                </div>
+                <div class="spacer"><h4><span class="glyphicon glyphicon-th-list"></span> Properties Detail</h4>
+                    <p> <?=$model->description ?></p>
+                </div>
+                <div><h4><span class="glyphicon glyphicon-map-marker"></span> Location</h4>
+                    <div class="well"><iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="../../../maps.google.com/fi000001.002642&t=m&z=14&output=embed" ></iframe></div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="col-lg-12  col-sm-6">
+                    <div class="property-info">
+                        <p class="price">$ <?=$model->price ?></p>
+                        <p class="area"><span class="glyphicon glyphicon-map-marker"></span> <?=$model->address ?></p>
+                        <div class="profile">
+                            <span class="glyphicon glyphicon-user"></span> Agent Details
+                            <p><?=$model->user->email ?><br><?=$model->user->username ?></p>
+                        </div>
+                    </div>
+                    <h6><span class="glyphicon glyphicon-home"></span> Availabilty</h6>
+                    <div class="listing-detail">
+                        <span data-toggle="tooltip" data-placement="bottom" data-original-title="Bed Room"><?=$model->bedroom ?></span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Living Room"><?=$model->livingroom ?></span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Parking"><?=$model->parking ?></span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Kitchen"><?=$model->kitchen ?></span> </div>
+                </div>
+                <div class="col-lg-12 col-sm-6 ">
+                    <div class="enquiry">
+                        <h6><span class="glyphicon glyphicon-envelope"></span> Post Enquiry</h6>
+                        <?php
+                        $form = \yii\bootstrap\ActiveForm::begin();
+                        ?>
+                        <?=$form->field($model_feedback,'email')->textInput(['value' => $current_user['email'], 'placeholder' => 'you@yourdomain.com'])->label(false) ?>
+                        <?=$form->field($model_feedback,'name')->textInput(['value' => $current_user['username'], 'placeholder' => 'Username'])->label(false) ?>
+                        <?=$form->field($model_feedback,'text')->textarea(['rows' => 6, 'placeholder' => 'Whats on your mind?'])->label(false) ?>
+                        <button type="submit" class="btn btn-primary" name="Submit">Send Message</button>
+
+                        <?php
+                        \yii\bootstrap\ActiveForm::end();
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
