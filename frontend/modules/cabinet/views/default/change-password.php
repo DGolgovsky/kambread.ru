c1b6564ba41a23b102d53c7fe7b1d3b91869a8cb
Dmitry Golgovsky <d.westcoast@aol.com>
2016-03-28 15:52:42 +0300

    profile modifications

--- frontend/modules/cabinet/views/default/change-password.php
+++ frontend/modules/cabinet/views/default/change-password.php
@@ -1,9 +1,19 @@
+<?php
+$this->title = 'Смена пароля';
+$this->params['breadcrumbs'][] = ['label' => 'Кабинет', 'url' => ['/cabinet']];
+$this->params['breadcrumbs'][] = ['label' => 'Профиль', 'url' => ['settings']];
+$this->params['breadcrumbs'][] = 'Пароль';
+?>
 <div class="product-form">
+    <h2 style="text-align: center"><small>Здесь Вы можете поменять пароль пользователя для входа. Просто введите его ниже.</small></h2>
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2"
     <?php $form = \yii\bootstrap\ActiveForm::begin(); ?>
 
     <?=$form->field($model,'password')->passwordInput() ?>
     <?=$form->field($model,'repassword')->passwordInput() ?>
 
-    <?= \yii\helpers\Html::submitButton('Change password', ['class' => 'btn btn-primary']) ?>
+    <?= \yii\helpers\Html::submitButton('Сохранить', ['class' => 'btn btn-primary']) ?>
     <?php \yii\bootstrap\ActiveForm::end() ?>
+    </div></div>
 </div>
--- frontend/modules/cabinet/views/default/settings.php
+++ frontend/modules/cabinet/views/default/settings.php
@@ -3,32 +3,35 @@ $this->title = $model->name;
 $this->params['breadcrumbs'][] = ['label' => 'Кабинет', 'url' => ['/cabinet']];
 $this->params['breadcrumbs'][] = 'Профиль';
 ?>
-
-<div class="user-form">
-    <?php
-    $form = \yii\bootstrap\ActiveForm::begin([
-        'options' => ['enctype' => 'multipart/form-data']
-    ]);
-    ?>
-
-    <?=\yii\helpers\Html::img(\common\components\UserComponent::getUserImage(\Yii::$app->user->id), ['width' => 200]) ?>
-
-    <?=$form->field($model,'name')->textInput(['autofocus' => true]) ?>
-    <?=$form->field($model,'username') ?>
-    <?=$form->field($model,'email') ?>
-    <?=$form->field($model,'phone') ?>
-    <?=\yii\helpers\Html::label('Avatar') ?>
-    <?=\yii\helpers\Html::fileInput('avatar') ?>
-
-    <div class="row">
-        <div class="col-lg-4 col-sm-4 col-xs-4">
-            <?= \yii\helpers\Html::a('Изменить пароль', ['/cabinet/change-password'], ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-        </div>
-        <div class="col-lg-4 col-lg-offset-4 col-sm-4 col-sm-offset-4 col-xs-4 col-xs-offset-4">
-            <?= \yii\helpers\Html::submitButton('Сохранить', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+<div class="row">
+    <div class="col-lg-8 col-md-8 col-sm-8">
+        <?php
+        $form = \yii\bootstrap\ActiveForm::begin([
+            'options' => ['enctype' => 'multipart/form-data']
+        ]);
+        ?>
+        <?=$form->field($model,'name')->textInput(['autofocus' => true]) ?>
+        <?=$form->field($model,'username') ?>
+        <?=$form->field($model,'email') ?>
+        <?=$form->field($model,'phone') ?>
+        <?=\yii\helpers\Html::label('Фото профиля') ?>
+        <?=\yii\helpers\Html::fileInput('avatar') ?>
+    </div>
+    <div class="col-lg-4 col-md-4 col-sm-4">
+        <?=\yii\helpers\Html::img(\common\components\UserComponent::getUserImage(\Yii::$app->user->id), ['width' => 200, 'class' => 'img-circle center-block img-responsive']) ?>
+        <hr>
+        <div class="bg-info center-block" style="width: 20em; padding: 10px;">
+            <h3><small>Внимание!</small></h3>
+            <p>На текущий момент пароль от сайта и от почтового ящика различаются. Для уточнения пароля от email свяжитесь с <a href="mailto:support@kambread.ru" target="_self">поддержкой</a> сайта.</p>
         </div>
     </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-sm-4 col-xs-4">
+        <?= \yii\helpers\Html::a('Изменить пароль', ['/cabinet/change-password'], ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+    <div class="col-lg-4 col-sm-4 col-xs-4">
+        <?= \yii\helpers\Html::submitButton('Сохранить', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
     <?php \yii\bootstrap\ActiveForm::end() ?>
-    <br>
-    <br>
 </div>
